680dd1e98be20342ad0c6ecf8aeb3bbd7e2f84da
Nikul <Nikul>
2021-03-24 11:07:50 +0530

    login page resolved

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,13 +35,18 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('guest')->except('logout');
-        $response = [
-            config('api.CODE')    => config('HttpCodes.accessDenied'),
-            config('api.MESSAGE') => config('Messages.invalidCredentials'),
-            config('api.RESULT')  => []
-        ];
-        ParcelHelper::sendResponse($response,config('HttpCodes.success'));
-        exit;
+        $currentURL = url()->current();
+        if (strpos($currentURL, 'api') !== false) { 
+            $response = [
+                config('api.CODE')    => config('HttpCodes.accessDenied'),
+                config('api.MESSAGE') => config('Messages.invalidCredentials'),
+                config('api.RESULT')  => []
+            ];
+            ParcelHelper::sendResponse($response,config('HttpCodes.success'));
+            exit;
+        } else {
+            $this->middleware('guest')->except('logout');    
+        }
+        
     }
 }
