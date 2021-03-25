8bbe75aa4334b50ea31026bcffc7dc3c7e300ff4
nikultaka <nikultaka@gmail.com>
2021-03-25 12:49:10 +0530

    container list change

--- app/Http/Controllers/ContainerlistController.php
+++ app/Http/Controllers/ContainerlistController.php
@@ -18,38 +18,39 @@ use DB;
 class ContainerlistController extends Controller
 {
     public function index(){
-    	return view('container\container_list');
+    	return view('container/container_list');
     }
 
     public function getListContainer(Request $request){
-        // $containerData  = Container::all()->toArray();
         $data = array();
         $containerData = $request->all();
         $getDatasql = DB::table('containers');
-      
-        if(isset($containerData['search']['value']) && $containerData['search']['value'] != '')
-        {
+		$getDatasql->select('containers.*', 'container_type.container_type as container_type')
+				   ->leftjoin("container_type", "container_type.id", "=", "containers.container_type");
+
+        if(isset($containerData['search']['value']) && $containerData['search']['value'] != ''){
             $search = $containerData['search']['value'];
             $getDatasql->where('containers.container_type','like','%'.$search.'%')
                        ->orWhere('containers.id','like','%'.$search.'%')
 			           ->orWhere('containers.container_number','like','%'.$search.'%')
 			           ->orWhere('containers.weight','like','%'.$search.'%')
 			           ->orWhere('containers.pin','like','%'.$search.'%')
+			           ->orWhere('containers.license_plate','like','%'.$search.'%')
 			           ->orWhere('containers.reference','like','%'.$search.'%')
 			           ->orWhere('containers.leeg','like','%'.$search.'%')
 			           ->orWhere('containers.created_at','like','%'.$search.'%');
-        }
+        }	
         $columns = array(
 			0 => 'containers.id',
 			1 => 'containers.reference',
 			2 => 'containers.created_at',
 			3 => 'containers.pin',
-			4 => 'containers.id',
+			4 => 'containers.license_plate',
 			5 => 'containers.container_number',
 			6 => 'containers.container_type',
 			7 => 'containers.leeg',
-			8 => 'containers.id',
-			9 => 'containers.id'
+			8 => 'containers.adr',
+			9 => 'containers.genset'
 		);
         if(isset($containerData['order'][0]['column']) && $containerData['order'][0]['column'] != '') {
             $order_by = '';
@@ -77,14 +78,22 @@ class ContainerlistController extends Controller
         foreach ($listall as $key => $row) {
 			$temp['ticker_number'] = $row->id;
 			$temp['reference'] = $row->reference;
-			$temp['date_time'] = $row->created_at;
+			$temp['date_time'] = $row->created_at != '' ? date('d-m-Y h:i', strtotime($row->created_at)) : '';
 			$temp['Pincode'] = $row->pin;
-			$temp['lisence_plate'] = $row->id;
+			$temp['lisence_plate'] = $row->license_plate;
 			$temp['container_number'] = $row->container_number;
 			$temp['container_type'] = $row->container_type;
 			$temp['vol_leeg'] = $row->leeg;
-			$temp['adr'] = '<input type="checkbox" class="form-check-input">';
-			$temp['genset'] = '<input type="checkbox" class="form-check-input">';
+			$adrstatus = '';
+			if($row->adr == 1){
+				$adrstatus = 'checked';
+			}
+			$temp['adr'] = '<input type="checkbox" class="form-check-input" '.$adrstatus.'>';
+			$gensetstatus = '';
+			if($row->genset != ''){
+				$gensetstatus = 'checked';
+			}
+			$temp['genset'] = '<input type="checkbox" class="form-check-input" '.$gensetstatus.'>';
 			$data[] = $temp;
 		}
         $json_data = array(
