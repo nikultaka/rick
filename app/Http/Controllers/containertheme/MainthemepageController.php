74406f6d4533421da81b9ccdf9e1853821a3f942
Nikul <Nikul>
2021-04-05 15:27:36 +0530

    new auth changes

--- app/Http/Controllers/containertheme/MainthemepageController.php
+++ app/Http/Controllers/containertheme/MainthemepageController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers\containertheme;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class MainthemepageController extends Controller
 {
-    public function login(){
+    public function login() {
+        if (Auth::user()) {
+            return redirect('/dashboard-theme');
+        } 
     	return view('containerlayouts/login');
     }
 
     public function forgotpassword(){
+        if (Auth::user()) {
+            return redirect('/dashboard-theme');
+        }
     	return view('containerlayouts/getemail');
     }
 
@@ -19,8 +26,11 @@ class MainthemepageController extends Controller
     }
 
     public function newpassword($token){
+        if (Auth::user()) {
+            return redirect('/dashboard-theme');
+        }
         $data['token'] = $token;
-		 return view('containerlayouts/forgotpassword', $data);
+		return view('containerlayouts/forgotpassword', $data);
     }
 
     
