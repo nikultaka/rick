e3655b25ef2c28246637ce72d97cd1d849276440
nikultaka <nikultaka@gmail.com>
2021-04-07 18:58:07 +0530

    handaling status add modal and delete done input

--- resources/views/container/corporate_information.blade.php
+++ resources/views/container/corporate_information.blade.php
@@ -14,28 +14,29 @@
         <h3>Address</h3>
         <form  method="post" class="form-horizontal" id="corporateform" name="corporateform">
         {{ csrf_field() }}
-            <div class="form-group">
-                <div>
+        
+            <div class="form-group row">
+                <div  class="col-xs-4">
                      <input type="text" class="form-control" id="address1" placeholder="address1" name="address1" value="{{isset($data->address1) ? $data->address1 : ''}}">
                 </div>
             </div>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                      <input type="text" class="form-control" id="address2" placeholder="address2" name="address2" value="{{isset($data->address2) ? $data->address2 : ''}}">
                 </div>
             </div>
             <div class="form-group ">
-                <div>
+                <div class="col-xs-4">
                     <input type="text" class="form-control" id="state" placeholder="state" name="state" value="{{isset($data->state) ? $data->state : ''}}">
                 </div>
             </div>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                     <input type="text" class="form-control" id="city" placeholder="city" name="city" value="{{isset($data->city) ? $data->city : ''}}">
                 </div>
             </div>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                       <input type="text" class="form-control" id="vatnumber" placeholder="vat number" name="vatnumber" value="{{isset($data->vatnumber) ? $data->vatnumber : ''}}">
                 </div>
             </div>
@@ -47,17 +48,17 @@
         <hr class="solid">
         <h3>Email Settings</h3>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                     <input type="email" class="form-control" id="weighingslipsemail" placeholder="Weighing Slips Email" name="weighingslipsemail" value="{{isset($data->weighingslipsemail) ? $data->weighingslipsemail : ''}}">
                 </div>
             </div>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                      <input type="email" class="form-control" id="storageemail" placeholder="Storage Email" name="storageemail" value="{{isset($data->storageemail) ? $data->storageemail : ''}}">
                 </div>
             </div>
             <div class="form-group">
-                <div>
+                <div class="col-xs-4">
                     <input type="email" class="form-control" id="invoiceemail" placeholder="Invoice Email" name="invoiceemail" value="{{isset($data->invoiceemail) ? $data->invoiceemail : ''}}">
                 </div>
             </div>
--- resources/views/container/handlingstatus.blade.php
+++ resources/views/container/handlingstatus.blade.php
@@ -13,7 +13,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h3 class="modal-title" id="handlingModalLabel">Update handaling status data</h3>
+        <h3 class="modal-title" id="handlingModalLabel">Update handaling</h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
